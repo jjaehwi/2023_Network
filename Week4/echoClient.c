bbe6a356337b182e4e208fff2aa7e9e0188dd391
jjaehwi <dlwognl99@naver.com>
2023-04-12 17:04:18 +0000

    Assignment2: UDPechoServer/Client

--- Week4/echoClient.c
+++ Week4/echoClient.c
@@ -7,7 +7,9 @@
 #include <stdlib.h>
 #include <arpa/inet.h>
 
+// 에러 핸들링을 위한 함수
 void errProc(const char *);
+
 int main(int argc, char **argv)
 {
     int mySock, readLen, nSent, nRecv;
@@ -18,6 +20,8 @@ int main(int argc, char **argv)
 
     mySock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
 
+    // 서버의 주소 구조는 초기화되어 destAddr 구조체에 저장
+    // 서버의 IP 주소는 명령줄 인수에서 가져오고 inet_addr을 사용하여 이진 형식으로 변환
     memset(&destAddr, 0, sizeof(destAddr));
     destAddr.sin_addr.s_addr = inet_addr(argv[1]);
     destAddr.sin_family = AF_INET;
@@ -26,25 +30,36 @@ int main(int argc, char **argv)
 
     while (1)
     {
+        // fgets를 사용하여 사용자로부터 입력을 읽고 sendto를 사용하여 서버로 보낸다.
         fgets(buff, BUFSIZ - 1, stdin);
         readLen = strlen(buff);
+        // 보낸 바이트 수는 nSent에 저장
         nSent = sendto(mySock, buff, readLen, 0,
                        (struct sockaddr *)&destAddr, addrLen);
+        // 전송 중 오류가 발생하면 해당 오류 메시지와 함께 errProc 함수가 호출
         if (nSent == -1)
             errProc("write");
+        // 서버로부터의 응답은 recvfrom을 사용하여 수신되고 buff에 저장
+        // 받은 바이트 수는 nRecv에 저장
         nRecv = recvfrom(mySock, buff, BUFSIZ - 1, 0,
                          (struct sockaddr *)&destAddr, &addrLen);
+        // 수신 중 오류가 발생하면 해당 오류 메시지와 함께 errProc 함수가 호출
         if (nRecv == -1)
             errProc("read");
         buff[nRecv] = '\0';
+        // 받은 메시지는 출력
         printf("Server: %s\n", buff);
+        // 수신된 메시지가 "END"이면 루프가 종료, 루프는 사용자가 "END"를 입력할 때까지 계속된다.
         if (!strcmp(buff, "END"))
             break;
     }
+    // close 시스템 호출을 사용하여 소켓을 닫고 프로그램을 종료
     close(mySock);
     return 0;
 }
 
+// errProc 함수는 strerror를 사용하여 얻은 오류 번호 및 설명과 함께 콘솔에 오류 메시지를 출력
+// 오류가 발생하면 프로그램은 상태 코드 1로 종료
 void errProc(const char *str)
 {
     fprintf(stderr, "%s: %s \n", str, strerror(errno));
--- Week4/echoServer.c
+++ Week4/echoServer.c
@@ -7,35 +7,46 @@
 #include <stdlib.h>
 #include <arpa/inet.h>
 
+// 에러 핸들링을 위한 함수
 void errProc(const char *);
 int main(int argc, char **argv)
 {
+    // 변수: 소켓, 클라이언트 연결 및 데이터 전송을 위한 변수를 초기화
     int mySock, readLen, nRecv, res;
     char buff[BUFSIZ];
     char *strAddr;
     struct sockaddr_in srcAddr, destAddr;
     socklen_t addrLen;
 
+    // 사용자가 올바른 인수를 입력했는지 확인 (수신할 포트 번호)
     if (argc != 2)
     {
         fprintf(stderr, "Usage: %s Port", argv[0]);
         return 0;
     }
+
+    // 소켓 생성: socket() 함수를 사용하여 UDP 소켓을 생성
     mySock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
     if (mySock == -1)
         errProc("socket");
+
+    // 소켓 주소 초기화: 서버 주소 정보로 sockaddr_in 구조체를 초기화
     memset(&srcAddr, 0, sizeof(srcAddr));
     srcAddr.sin_addr.s_addr = htonl(INADDR_ANY);
     srcAddr.sin_family = AF_INET;
     srcAddr.sin_port = htons(atoi(argv[1]));
 
+    // 로컬 주소 및 포트에 소켓 바인딩: bind() 함수를 사용하여 sockaddr_in 구조에 지정된 로컬 주소 및 포트에 소켓을 바인딩
     res = bind(mySock, (struct sockaddr *)&srcAddr,
                sizeof(srcAddr));
     if (res == -1)
         errProc("bind");
     addrLen = sizeof(destAddr);
+
+    // recvfrom 함수를 사용하여 메시지를 계속 수신하고 sendto 함수를 사용하여 보낸 사람에게 다시 보내는 무한 루프
     while (1)
     {
+        // 클라이언트로부터 메세지를 receive
         nRecv = recvfrom(mySock, buff, BUFSIZ - 1, 0,
                          (struct sockaddr *)&destAddr,
                          &addrLen);
@@ -45,12 +56,17 @@ int main(int argc, char **argv)
             buff[nRecv - 1] = '\0';
         else
             buff[nRecv] = '\0';
+
+        // sender 의 IP 주소와 포트 번호를 얻는다.
         strAddr = inet_ntoa(destAddr.sin_addr);
+        // 받은 메세지와 sender 의 IP 주소와 포트를 출력한다.
         printf("%s:%d>%s\n", strAddr, ntohs(destAddr.sin_port), buff);
+        // Sender 에게 다시 메세지를 send 한다.
         nRecv = strlen(buff);
         sendto(mySock, buff, nRecv, 0,
                (struct sockaddr *)&destAddr, addrLen);
     }
+    // 소켓을 닫는다.
     close(mySock);
     return 0;
 }
